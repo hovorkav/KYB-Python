488ee865e1f72dc2d4c3cb3af9a5b171fc680bb5
D32\zak <spseplzen@spseplzen.cz>
2025-10-03 12:26:24 +0200

    matik #2

--- main.py
+++ main.py
@@ -9,7 +9,12 @@ samopopisne_nazvy_promennnych = "ahoj"
 print(samopopisne_nazvy_promennnych)
 print(type(samopopisne_nazvy_promennnych))
 #type vraci class promene (zde je class string, 0.5 by bylo float)
-import math
 cislo = 9
-desetinne_cislo = math.sqrt(2)
+desetinne_cislo = 2**(1/2)
 print(cislo, type(cislo), desetinne_cislo, type(desetinne_cislo))
+
+#zaokrouhleni: round()
+# % - zbytek deleni (11%3 => 2)
+
+logicka = True
+l2 = False
