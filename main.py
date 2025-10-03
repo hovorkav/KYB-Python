a7aa868841bcd5e780bf5757b68fa0e15f7838b4
D32\zak <spseplzen@spseplzen.cz>
2025-10-03 12:09:01 +0200

    matik

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 print("Hello World!")
-print("Zulul")
+a = "asd"
+b = a
+
+print(b)    #komentar
+
+#DATOVE TYPY V PYTHONU
+samopopisne_nazvy_promennnych = "ahoj"
+print(samopopisne_nazvy_promennnych)
+print(type(samopopisne_nazvy_promennnych))
+#type vraci class promene (zde je class string, 0.5 by bylo float)
+import math
+cislo = 9
+desetinne_cislo = math.sqrt(2)
+print(cislo, type(cislo), desetinne_cislo, type(desetinne_cislo))
